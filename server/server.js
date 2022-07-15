ed7ff60e4a1529d2dedd9c41bb7c3fa448c18264
David Brown <davidbrown@davids-MacBook-Pro.local>
2022-07-15 15:57:43 -0500

    completed projects backend

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const ProjectsRouter = require('./routes/projectsrouter.js');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/projects',ProjectsRouter)
 
 // Serve static files
 app.use(express.static('build'));
