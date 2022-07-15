d6c3fda24a22e19d598e9b1da747f917fcc86af3
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2022-07-15 15:36:45 -0500

    did activity and employee_activity routes

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const activityRouter = require('./routes/activity.router')
+const activity_employeeRouter= require('./routes/activity_employee.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +25,8 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/activity', activityRouter)
+app.use('/api/activity_employee', activity_employeeRouter)
 
 // Serve static files
 app.use(express.static('build'));
